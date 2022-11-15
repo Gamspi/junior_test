4978e57e398913c73bdbb17751d844dc7f4d4fb5
Gamspi <niko.tatt007@gmail.com>
2022-11-15 16:34:25 +0700

    #feat added close for filter

--- src/ts/FilterController.ts
+++ src/ts/FilterController.ts
@@ -7,18 +7,29 @@ export class FilterController {
     this.modal = container.querySelector('.j-filter-modal')
     this.closeBtn = container.querySelector('.j-filter-close')
     this.openBtn = container.querySelector('.j-filter-open')
+    this.ListenerBody = this.ListenerBody.bind(this)
   }
 
   init () {
     if (this.closeBtn) {
       this.closeBtn.onclick = () => {
         this.modal?.classList.remove('_open')
+        document.body.removeEventListener('click', this.ListenerBody)
       }
     }
     if (this.openBtn) {
       this.openBtn.onclick = () => {
         this.modal?.classList.add('_open')
+        document.body.addEventListener('click', this.ListenerBody)
       }
+    }
+  }
+
+  ListenerBody (e: Event) {
+    const target = e.target as HTMLElement
+    if (this.modal && !target.closest('.j-filter')) {
+      this.modal.classList.remove('_open')
+      document.body.removeEventListener('click', this.ListenerBody)
     }
   }
 }
