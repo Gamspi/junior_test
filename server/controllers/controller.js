7f154b33fe65d5df8a65a820470328e618c98a7b
Gamspi <niko.tatt007@gmail.com>
2022-11-16 15:07:17 +0700

    #feat added delay for server functions

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,82 +1,89 @@
 let bd = require('../bd/bd')
 let users = require('../bd/usersBD')
+const DELAY = 500
 
 class GenreController {
   async filter (req, res) {
-    let { id } = req.query
-    id = id.toLowerCase()
-    if (id) {
-      if (id === 'all') {
-        res.json({
-          data: bd.sort((a, b) => a.id - b.id),
-          status: 'success',
-          message: ''
-        })
-      } else {
-        const value = bd.filter((item) => {
-          return item.genres.includes(id)
-        })
-        res.json({
-          data: value.sort((a, b) => a.id - b.id),
-          status: 'success',
-          message: ''
-        })
+    setTimeout(() => {
+      let { id } = req.query
+      id = id.toLowerCase()
+      if (id) {
+        if (id === 'all') {
+          res.json({
+            data: bd.sort((a, b) => a.id - b.id),
+            status: 'success',
+            message: ''
+          })
+        } else {
+          const value = bd.filter((item) => {
+            return item.genres.includes(id)
+          })
+          res.json({
+            data: value.sort((a, b) => a.id - b.id),
+            status: 'success',
+            message: ''
+          })
+        }
       }
-    }
+    }, DELAY)
   }
 }
 
 class LikeController {
   async handelLike (req, res) {
-    const { id, user: userId } = req.body
-    console.log(userId)
-    const elem = bd.find(elem => elem.id === id)
-    if (elem) {
-      if (userId) {
-        const user = users.find(user => user.id === userId)
-        if (!elem.isLike) {
-          users = [...users.filter(user => user.id !== userId), { ...user, favorite: [...user.favorite, id] }]
-        } else {
-          users = [...users.filter(user => user.id !== userId), {
-            ...user,
-            favorite: [...user.favorite.filter(f => f !== id)]
-          }]
+    setTimeout(() => {
+      const { id, user: userId } = req.body
+      console.log(userId)
+      const elem = bd.find(elem => elem.id === id)
+      if (elem) {
+        if (userId) {
+          const user = users.find(user => user.id === userId)
+          if (!elem.isLike) {
+            users = [...users.filter(user => user.id !== userId), { ...user, favorite: [...user.favorite, id] }]
+          } else {
+            users = [...users.filter(user => user.id !== userId), {
+              ...user,
+              favorite: [...user.favorite.filter(f => f !== id)]
+            }]
+          }
         }
-      }
 
-      bd = [...bd.filter(elem => elem.id !== id), { ...elem, isLike: !elem.isLike }]
-      return res.json({
-        status: 'success',
-        data: { ...elem, isLike: !elem.isLike, user: users.find(user => user.id === userId) },
-        message: ''
-      })
-    } else {
-      return res.status(400).json({
-        message: 'Not find',
-        status: 'error',
-        data: {}
-      })
-    }
+        bd = [...bd.filter(elem => elem.id !== id), { ...elem, isLike: !elem.isLike }]
+        return res.json({
+          status: 'success',
+          data: { ...elem, isLike: !elem.isLike, user: users.find(user => user.id === userId) },
+          message: ''
+        })
+      } else {
+        return res.status(400).json({
+          message: 'Not find',
+          status: 'error',
+          data: {}
+        })
+      }
+    }, DELAY)
   }
 }
 
 class Authorization {
   async auth (req, res) {
-    const { email, password } = req.body
-    const user = users.find(user => user.email === email && user.password === password)
-    if (user) {
-      return res.status(200).json({
-        status: 'success',
-        data: { id: user.id },
-        message: ''
-      })
-    } else {
-      return res.status(400).json({
-        message: 'Not find',
-        status: 'error',
-        data: {}
-      })
-    }
+    setTimeout(() => {
+      const { email, password } = req.body
+      const user = users.find(user => user.email === email && user.password === password)
+      if (user) {
+        return res.status(200).json({
+          status: 'success',
+          data: { id: user.id },
+          message: ''
+        })
+      } else {
+        return res.status(400).json({
+          message: 'Not find',
+          status: 'error',
+          data: {}
+        })
+      }
+    }, 500)
   }
 }
 
@@ -104,28 +111,30 @@ class Filter {
         data: resolve,
         message: ''
       })
-    }, 500)
+    }, DELAY)
   }
 }
 
 class Search {
   async find ({ body: { value } }, res) {
     console.log({ value })
-    if (value) {
-      res.json({
-        status: 'success',
-        data: `Ты написал "${value}", что это значит?`,
-        message: ''
-      })
-    } else {
-      res.status(400).json(
-        {
-          status: 'error',
-          data: null,
-          message: 'Ты забыл мне написать =('
-        }
-      )
-    }
+    setTimeout(() => {
+      if (value) {
+        res.json({
+          status: 'success',
+          data: `Ты написал "${value}", что это значит?`,
+          message: ''
+        })
+      } else {
+        res.status(400).json(
+          {
+            status: 'error',
+            data: null,
+            message: 'Ты забыл мне написать =('
+          }
+        )
+      }
+    }, DELAY)
   }
 }
 
