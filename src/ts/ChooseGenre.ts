445f07e3098e4316958610481d6f73c3def7383b
Gamspi <niko.tatt007@gmail.com>
2022-11-16 16:24:59 +0700

    #feat added disabled for sound tabs

--- src/ts/ChooseGenre.ts
+++ src/ts/ChooseGenre.ts
@@ -17,23 +17,26 @@ export class ChooseGenre extends SoundItemGeneration {
     this.toggleActive(this.tabs[0])
     this.tabs.forEach(tab => {
       tab.onclick = e => {
-        const target = e.target as HTMLElement
+        const target = e.target as HTMLButtonElement
+        target.disabled = true
         const id = target.dataset.id
         this.toggleActive(target)
         if (id) {
-          this.GetGenre(id)
+          this.GetGenre(id, () => { target.disabled = false })
         }
       }
     })
   }
 
-  GetGenre (id?:string) {
+  GetGenre (id?:string, finallyFunc?: ()=>void) {
     Xhr.Get<MyRequest<MusicTrack[]>>('http://localhost:5000/api/genre', {
       id
     }).then(({ data }) => {
       if (this.list) this.list.innerHTML = ''
       data.forEach(item => this.itemGeneration(item))
       this.itemInit()
+    }).finally(() => {
+      if (finallyFunc) finallyFunc()
     })
   }
 
