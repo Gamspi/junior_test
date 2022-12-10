01b430df5cb4fa79b02bcb95aadb09ea54608df1
Gamspi <niko.tatt007@gmail.com>
2022-12-10 16:37:15 +0700

    feat added animation for sounds list

--- src/ts/audioObserver.ts
+++ src/ts/audioObserver.ts
@@ -1,24 +1,36 @@
 import { ButtonClick } from './buttonClick'
 import { AudioController } from './audioController'
 import { ClassEnums } from './utils/enums/classEnums'
+
 export class AudioObserver {
+  private height = 0
+
   constructor (private container: HTMLElement) {
   }
 
   init () {
+    this.changeParentHeight()
     const observer = new MutationObserver(this.observerCallback)
     observer.observe(this.container, {
       childList: true
     })
   }
 
+  changeParentHeight = () => {
+    this.container.parentElement!.style.height = this.container.offsetHeight / 10 + 'rem'
+  }
+
   observerCallback: MutationCallback = () => {
-    for (let i = 0; i < this.container.children.length; i++) {
-      const target = this.container.children[i]
-      setTimeout(() => {
-        target.classList.add(ClassEnums.VISIBLE)
-      }, 50 * i)
-    }
+    this.changeParentHeight()
+    setTimeout(() => {
+      for (let i = 0; i < this.container.children.length; i++) {
+        const target = this.container.children[i]
+        setTimeout(() => {
+          target.classList.add(ClassEnums.VISIBLE)
+        }, 50 * i)
+      }
+    }, 200)
+
     this.container.querySelectorAll('.j-button-click')
       .forEach(block => {
         const buttonClick = new ButtonClick(block as HTMLButtonElement)
